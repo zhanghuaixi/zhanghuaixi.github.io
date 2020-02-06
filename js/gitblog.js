91343c1ff3e3c14ce18f98eb5bc18087543b8170
张淮汐 <49033099+zhanghuaixi@users.noreply.github.com>
2020-02-06 17:31:22 +0800

    Update gitblog.js

--- js/gitblog.js
+++ js/gitblog.js
@@ -619,7 +619,7 @@ var gitblog = function(config) {
                 }
                 data[i].body = data[i].body.replace(/<.*?>/g, "");
                 data[i].created_at = self.utc2localTime(data[i].created_at);
-                document.getElementById('issue-list').innerHTML += '<div class="container"><div class="row"><div class="col-lg-6"><div class="text-container"><h2><a href="content.html?id=' + data[i].number + '">' + data[i].title + '</a></h2><p class="issue">' + data[i].body + '</p><ul class="list-unstyled li-space-lg"><li class="media"><i class="fas fa-square"></i><div class="media-body">' + '作者｜' + data[i].user.login + '</div></li><li class="media"><i class="fas fa-square"></i><div class="media-body">' + '标签｜' + labels_content + '</div></li><li class="media"><i class="fas fa-square"></i><div class="media-body date">' + '时间｜' + data[i].created_at + '</div></li></ul></div></div></div></div>';
+                document.getElementById('issue-list').innerHTML += '<li><div id="details" class="basic-1"><div class="container"><div class="row"><div class="col-lg-6"><div class="text-container"><h2 class="title"><a href="content.html?id=' + data[i].number + '">' + data[i].title + '</a></h2><div class="excerpt"><p class="issue">' + data[i].body + '</p></div>' + '<ul class="list-unstyled li-space-lg meta"><li class="media"><i class="fas fa-square"></i><div class="media-body">' + data[i].user.login + '</div></li><li class="media"><i class="fas fa-square"></i><div class="media-body">' + labels_content + '</div></li><li class="media"><i class="fas fa-square"></i><div class="media-body date">' + data[i].created_at + '</div></li></ul></div></div></div></div></div></li>';
             }
         },
         show: function(request_url) {
